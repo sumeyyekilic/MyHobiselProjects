acc8287a6e40d2bd11dc61964bdd4a671d1208e3
sümeyye kılıç <smyyekilic@gmail.com>
2021-01-28 22:15:06 +0300

    hobisel eğlence

---
+++ hobisel5.py
@@ -0,0 +1,22 @@
+# *********************************************************
+#  **     @sumklc                                         **
+#  **     Sümeyye kılıç                                   **
+#  **     10.01.2021                                      **
+#  **     Hobisel -unutulan python bilgilerini tazeleme   **
+#  **     -SublimeText                                    **
+#  *********************************************************
+# ------- pip install emoji        ------                 **
+# ------- emoji kullandğm site:    ------                 **
+#         https://www.webfx.com/tools/emoji-cheat-sheet/  **
+# -------    :sparkles:            ------                 **
+#  *********************************************************
+
+
+import emoji
+
+resHeart = emoji.emojize('PYTHON LOVER    :blue_heart: ')
+resSun = emoji.emojize("The weather today     :sunny: ")
+
+if __name__ == '__main__':
+    print(resHeart)
+    print(resSun)
