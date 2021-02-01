a8fe51131acaf60d6d389f53976210c65a99d3e2
sümeyye kılıç <smyyekilic@gmail.com>
2021-02-02 00:02:25 +0300

    hobisel eğlence boolenn..

--- hobisel8.py
+++ hobisel8.py
@@ -1,14 +1,25 @@
-status=False
+# *********************************************************
+#  **     @sumklc                                         **
+#  **     Sümeyye kılıç                                   **
+#  **     01.02.2021                                      **
+# *********************************************************
 
+
+"""status param olarak varsayılan bir boolen değer atadım:"""
+status=False  #true da atayabilirsiniz..
+
+#bunu kullanarak if şartı yazarsam:
+
+#BİRİNCİ YÖNT
 if status:
     s1=1
 else:
     s1=0
 
-print("birinci yöntemde :" , s1)
-
-#***********************
-
+#İKİNCİ YÖNT.. (best)
 s2=1 if status else 0
 
-print("ikinci yöntemde :" , s2)
+
+if __name__ == '__main__':
+    print("birinci yöntemde :", s1)
+    print("ikinci yöntemde :" , s2)
