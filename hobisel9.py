b3e9c68b02b5e2ff45cfc87bdc42070c7d4bbe95
sümeyye kılıç <smyyekilic@gmail.com>
2021-02-03 00:30:55 +0300

    hobisel eğlence liste elemanlarına erişim..

---
+++ hobisel9.py
@@ -0,0 +1,31 @@
+#sıradan
+element=(1,2)
+print(element)
+
+#tek tek erişim
+x,y =(1,2)
+print(x)
+print(y)
+
+#deneme2
+x,_ =(1,2)
+print(x)
+#print(y)
+
+#deneme3
+
+x,y,*z =(1,2,3,4,5,6)
+print(x)
+print(y)
+print(z)
+
+#deneme4
+x,y,*z,t =(1,2,3,4,5,6)
+print(x)
+print(y)
+print(z)
+print(t)
+
+#z'yi yorum satırı yapssak dahi t hep en son değeri alacaktır
+
+
