1ae14061521f2d29a0e870062682b28cc9c8c39c
sümeyye kılıç <smyyekilic@gmail.com>
2021-02-04 23:55:42 +0300

    hobisel eğlence listeden beznersiz öğeleri al..

---
+++ hobisel12.py
@@ -0,0 +1,9 @@
+
+"""Bir listeden veya dizeden uniq öğeler almak istiyorum"""
+
+#1. listeden uniq ögeleri seçip sıralamak:
+karmasikList = [8, 8, 2, 3, 4, 1, 1, 1, 5, 5]
+
+if __name__ == '__main__':
+    print (set(karmasikList))
+
