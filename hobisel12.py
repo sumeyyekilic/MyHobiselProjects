abda2a3263f0ffd6a9d74c6f138254333b5ddd97
sümeyye kılıç <smyyekilic@gmail.com>
2021-02-04 23:58:42 +0300

    hobisel eğlence string dize uniq bul..

--- hobisel12.py
+++ hobisel12.py
@@ -4,6 +4,10 @@
 #1. listeden uniq ögeleri seçip sıralamak:
 karmasikList = [8, 8, 2, 3, 4, 1, 1, 1, 5, 5]
 
+#2 string dize öğelerinden uniq olanları bulmak:
+stringDize=("HİİİSMYYAAABBBCCC")
+
+
 if __name__ == '__main__':
     print (set(karmasikList))
-
+    print(set(stringDize))
