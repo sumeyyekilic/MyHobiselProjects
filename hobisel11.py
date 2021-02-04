ecf3f69dc6ef4de892692a1ca7af8b9fb3b6640d
sümeyye kılıç <smyyekilic@gmail.com>
2021-02-04 23:47:03 +0300

    hobisel eğlence reverse..

---
+++ hobisel11.py
@@ -0,0 +1,13 @@
+# *********************************************************
+#  **     @sumklc                                         **
+#  **     Sümeyye kılıç                                   **
+#  **     04.02.2021                                      **
+# *********************************************************
+"""ters stringler ve listeler oluşturabilmek"""
+
+tersString = "sümeyyeKılıç"[::-1]
+tersList = [2,3,7,8][::-1]
+
+if __name__ == '__main__':
+    print(tersString)
+    print(tersList)
